4d04fcfe22acb906f85803d3366be7c2839905b0
Rod Toll <rod.toll@gmail.com>
2014-01-26 17:49:31 +0000

    Updated to it runs on raspberry pi. Removed logging library dependency. Switch to device 0. Added device as thread parameter.

--- BlueTracker/Debug/objects.mk
+++ BlueTracker/Debug/objects.mk
@@ -4,5 +4,5 @@
 
 USER_OBJS :=
 
-LIBS := -lbluetooth -lcurl -lboost_thread -lpthread -lboost_log -lboost_date_time -lboost_system -lboost_chrono -lboost_log_setup
+LIBS := -lbluetooth -lcurl -lboost_thread -lpthread -lboost_date_time -lboost_system -lboost_chrono 
 
