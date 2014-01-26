b39ea6ac05298253ea5f4733927d9d2209f6c439
rodtoll <rod.toll@gmail.com>
2014-01-25 16:27:43 -0800

    Adding make files

---
+++ BlueTracker/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lbluetooth -lcurl -lboost_thread -lpthread -lboost_log -lboost_date_time -lboost_system -lboost_chrono -lboost_log_setup
+
